94548eb0b16bb371ac75e9e93b7d0d81c6f6bb84
ayushghosh-123 <ghoshayush910@gmail.com>
2025-08-31 10:17:16 +0530

    Implement authentication middleware and add initial pages for Home, SocialShare, SignIn, and SignUp

--- middleware.ts
+++ middleware.ts
@@ -18,24 +18,26 @@ export default clerkMiddleware(async (auth, req) => {
     const isAccesingDashboard = currentUrl.pathname === '/home'
     const isApiRequest =  currentUrl.pathname.startsWith("/api")
 
-
+    //if user is logged in and accesing a public route but nt he dashboard
     if(userId && isPublicRoute(req) && !isAccesingDashboard){
           return NextResponse.redirect(new URL("/home", req.url))
     }
 
     // not logged in
     if(!userId){
-       if(!isPublicRoute(req)){
-        
+      // If user is not logged in and trying to acess a protected route
+       if(!isPublicRoute(req) && !isPublicApiRoute(req)){
+            return NextResponse.redirect(new URL("/sign-in", req.url))
+       }
+      //  if the request is for a protected API and the user is not logged in 
+       if(isApiRequest && !isPublicApiRoute(req)){
+          return NextResponse.redirect(new URL('/sign-in', req.url))
        }
     }
+
+    return NextResponse.next()
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: [],
 }
